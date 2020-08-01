c5141ae410457b3e57200736322cab4e1510f2cb
Jan So <virgilio_so_jr@yahoo.com>
2020-08-01 20:09:15 +0800

    Prepare the name controller

---
+++ src/main/java/com/leadiro/starter/controller/NameCleanController.groovy
@@ -0,0 +1,30 @@
+package com.leadiro.starter.controller
+
+import com.leadiro.starter.service.name.NameCleanService
+import groovy.transform.CompileStatic
+import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@Slf4j
+@CrossOrigin
+@RestController
+@CompileStatic
+public class NameCleanController {
+    /**
+     * Use name clean service.
+     */
+    @Autowired
+    private NameCleanService nameCleanService;
+
+    @PostMapping(value = "/parse/name")
+    def parseName(
+            @RequestBody(required = true)
+            final List<String> names) {
+        return nameCleanService.processNames(names);
+    }
+}
+
---
+++ src/main/java/com/leadiro/starter/service/name/NameCleanService.java
@@ -0,0 +1,29 @@
+package com.leadiro.starter.service.name;
+
+import com.leadiro.starter.service.name.dto.NameDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Slf4j
+public class NameCleanService {
+    /**
+     * Process a list of names.
+     * @param names List of names
+     * @return Processed names;
+     */
+    public List<NameDto> processNames(final List<String> names) {
+        List<NameDto> nameDtos = new ArrayList<>();
+        for (String name : names) {
+            // TODO call a method here that cleanses the name
+            NameDto nameDto = new NameDto();
+            nameDto.setFirstName("FN" + name);
+            nameDto.setLastName("LN" + name);
+            nameDtos.add(nameDto);
+        }
+        return nameDtos;
+    }
+}
