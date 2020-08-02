793a3921fc1b3a7faa0e374315a082af3d666302
Jan So <virgilio_so_jr@yahoo.com>
2020-08-02 14:58:55 +0800

    Class to track the processing of names

---
+++ src/main/java/com/leadiro/starter/service/name/dto/NameProcessDto.java
@@ -0,0 +1,17 @@
+package com.leadiro.starter.service.name.dto;
+
+import lombok.Data;
+
+@Data
+public class NameProcessDto {
+
+    /**
+     * Name detail.
+     */
+    private NameDto name;
+
+    /**
+     * Processing.
+     */
+    private String processing;
+}
