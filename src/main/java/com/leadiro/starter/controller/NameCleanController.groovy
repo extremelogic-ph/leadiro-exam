04faa3fcb55a776e145e3f08aed7fabd3e5743d8
Jan So <virgilio_so_jr@yahoo.com>
2020-08-07 12:12:53 +0800

    Groovy allows it. Adding for compatibility

--- src/main/java/com/leadiro/starter/controller/NameCleanController.groovy
+++ src/main/java/com/leadiro/starter/controller/NameCleanController.groovy
@@ -1,13 +1,13 @@
 package com.leadiro.starter.controller
 
-import com.leadiro.starter.service.name.NameService
-import groovy.transform.CompileStatic
-import lombok.extern.slf4j.Slf4j
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import com.leadiro.starter.service.name.NameService;
+import groovy.transform.CompileStatic;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @CrossOrigin
--- src/main/java/com/leadiro/starter/controller/StarterController.groovy
+++ src/main/java/com/leadiro/starter/controller/StarterController.groovy
@@ -1,12 +1,12 @@
 package com.leadiro.starter.controller
 
-import com.leadiro.starter.service.StarterService
-import groovy.transform.CompileStatic
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import com.leadiro.starter.service.StarterService;
+import groovy.transform.CompileStatic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
